4c68ec411849de6faa2af356ba4b98e0de648def
Syedts <71299288+Syedts@users.noreply.github.com>
2020-11-17 19:16:34 -0500

    Home Page added

--- app/src/main/java/com/example/gangel/MainActivity.java
+++ app/src/main/java/com/example/gangel/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gangel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void signinClk(View view)
+    {
+        Intent signinBtn = new Intent(this, SignIn.class);
+        startActivity(signinBtn);
+
+        Toast.makeText(this, "Sign-in page", Toast.LENGTH_SHORT).show();
+    }
+    public void createClk(View view)
+    {
+        Intent createAcc = new Intent(this, SignIn.class);   //// Sign in till Sign up
+        startActivity(createAcc);
+
+        Toast.makeText(this, "Sign-up page", Toast.LENGTH_SHORT).show();
+    }
+    
 }
